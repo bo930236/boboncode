57247f69f815d63f9f4267e15febd8612ee0a431
q_tw_bobo <q_tw_bobo@t.emp>
2023-11-02 15:32:34 +0800

    feat: add CloudinaryVideo Component

--- src/components/images/CloudinaryVideo.tsx
+++ src/components/images/CloudinaryVideo.tsx
@@ -2,22 +2,17 @@ import clsx from 'clsx';
 import { CldVideoPlayer } from 'next-cloudinary';
 import * as React from 'react';
 
+import 'next-cloudinary/dist/cld-video-player.css';
+
 type CloudinaryVideoType = {
   videoPublicId: string;
   projectPath: string;
   height: string | number;
   width: string | number;
-  alt: string;
-  title?: string;
+  controls: boolean;
   className?: string;
-  preview?: boolean;
-  noStyle?: boolean;
-  aspect?: {
-    width: number;
-    height: number;
-  };
   mdx?: boolean;
-} & React.ComponentPropsWithoutRef<'figure'>;
+} & React.ComponentPropsWithoutRef<'video'>;
 
 export default function CloudinaryVideo({
   videoPublicId,
@@ -25,22 +20,16 @@ export default function CloudinaryVideo({
   height,
   width,
   className,
-  preview = true,
-  noStyle = false,
   mdx = false,
   style,
-  aspect,
   ...rest
 }: CloudinaryVideoType) {
-  const aspectRatio = aspect ? aspect.height / aspect.width : undefined;
-
   const RESIZE_MAX_WIDTH = 1000;
   const resizedToMaxWidth = mdx && +width >= RESIZE_MAX_WIDTH;
 
   return (
-    <figure
+    <video
       className={clsx(className, {
-        'overflow-hidden rounded shadow dark:shadow-none': !noStyle,
         'mx-auto w-full': mdx && +width <= 800,
       })}
       style={{
@@ -49,40 +38,28 @@ export default function CloudinaryVideo({
       }}
       {...rest}
     >
-      <div
-        style={{
-          position: 'relative',
-          height: 0,
-          paddingTop: aspectRatio
-            ? `${aspectRatio * 100}%`
-            : `${(+height / +width) * 100}%`,
-          cursor: preview ? 'zoom-in' : 'default',
-        }}
-      >
-        <div className='absolute left-0 top-0'>
-          <CldVideoPlayer
-            id={videoPublicId}
-            width={
-              resizedToMaxWidth ? Math.min(+width, RESIZE_MAX_WIDTH) : width
-            }
-            height={
-              resizedToMaxWidth ? (RESIZE_MAX_WIDTH * +height) / +width : height
-            }
-            src={projectPath}
-            transformation={{
-              crop: 'fill',
-              gravity: 'center',
-              width: resizedToMaxWidth
-                ? Math.min(+width, RESIZE_MAX_WIDTH)
-                : width,
-              height: resizedToMaxWidth
-                ? (RESIZE_MAX_WIDTH * +height) / +width
-                : height,
-            }}
-          />
-          ;
-        </div>
+      <div className='absolute left-0 top-0'>
+        <CldVideoPlayer
+          id={videoPublicId}
+          width={resizedToMaxWidth ? Math.min(+width, RESIZE_MAX_WIDTH) : width}
+          height={
+            resizedToMaxWidth ? (RESIZE_MAX_WIDTH * +height) / +width : height
+          }
+          controls={true}
+          src={projectPath}
+          transformation={{
+            crop: 'fill',
+            gravity: 'auto',
+            width: resizedToMaxWidth
+              ? Math.min(+width, RESIZE_MAX_WIDTH)
+              : width,
+            height: resizedToMaxWidth
+              ? (RESIZE_MAX_WIDTH * +height) / +width
+              : height,
+          }}
+        />
+        ;
       </div>
-    </figure>
+    </video>
   );
 }
